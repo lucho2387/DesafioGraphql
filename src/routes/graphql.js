c58dd6ecec94afc203cfd99f53bdae695bc01cc3
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-08-06 20:56:38 -0300

    Update graphql.js

--- src/routes/graphql.js
+++ src/routes/graphql.js
@@ -1,55 +1,25 @@
-const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
-const Producto = require('../models/model/product');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql')
+const { makeExecutableSchema }  = require('@graphql-tools/schema')
+const typeDefs = require('../models/model/graphql')
+const resolvers  = require('../models/Daos/resolvers')
+const router = express.Router();
 
-var schema = buildSchema(`
-    type Product {
-        id: String,
-        nombre: String,
-        descripcion: String,
-        codigo: Int,
-        imagen: String,
-        precio: Int,
-        stock: Int
-    },
-    type Query {
-        products: [Product]
-    },
-    type Mutation {
-        storeProduct(nombre: String!, descripcion: String!, codigo: Int!, imagen: String!, precio: Int!, stock: Int!,): Product,
-        productById(id: String): Product,
-        deleteProductById(id: String): Product
-    },
-`);
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers
+})
 
-const storeProduct = async function ({ nombre, descripcion, codigo,  imagen, precio, stock }) {
-    let product = { nombre, descripcion, codigo,  imagen, precio, stock };
-    return await Producto.create(product);
-};
+class GraphqlRouter{
+    constructor() {
+    }
+    start() {
+        router.use('/graphql', graphqlHTTP({
+            schema,
+            graphiql: true
+         }));
+        return router
+    }
+} 
 
-const products = async function () {
-    return await Producto.find();
-}
-
-const productById = async function ({ id }) {
-    return await Producto.findById(id);
-}
-
-const deleteProductById = async function ({id}) {
-    return await Producto.delete(id);
-}
-
-const root = {
-    products: products,
-    storeArticle: storeProduct,
-    productById: productById,
-    deleteProductById: deleteProductById
-};
-
-module.exports.start = function () {
-    return graphqlHTTP({
-        schema: schema,
-        rootValue: root,
-        graphiql: true
-    });
-}
+module.exports = GraphqlRouter 
