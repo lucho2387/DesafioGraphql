020e304b1c0a1fa277d8c6f6a40d2fb5ca7d7171
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-08-06 20:59:58 -0300

    Update productsDBMongo.js

--- src/models/Daos/productsDBMongo.js
+++ src/models/Daos/productsDBMongo.js
@@ -1,10 +1,10 @@
 const productDTO = require('../dto/products')
 const ProductsBaseDAO = require('./products') 
-const ProductoSchema = require('../model/product')
+const Product = require('../model/product')
 
 class ProductsDBMongo extends ProductsBaseDAO {
     constructor() {
-        super('productos', ProductoSchema)
+        super('productos', Product)
     }
 }
 
