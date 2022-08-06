bc79a15d8829a7dfb961e49b7b166f79986f98e8
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-08-06 20:57:45 -0300

    Update graphql.js

--- src/models/model/graphql.js
+++ src/models/model/graphql.js
@@ -1,63 +1,23 @@
-const { buildSchema } = require('graphql')
-
-class Producto{
-    constructor(id, { nombre, descripcion, codigo, imagen, precio,stock}) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.codigo = codigo;
-        this.imagen = imagen;
-        this.precio = precio;
-        this.stock = stock;
+const typeDefs =`
+    type Product {
+        _id: String!
+        nombre: String
+        descripcion: String
+        codigo: String
+        imagen: String
+        precio: Int
+        stock: Int
     }
-}
 
-// const schema = buildSchema(`
-// type Persona {
-//   id: ID!
-//   nombre: String,
-//   edad: Int
-// }
-// input PersonaInput {
-//   nombre: String,
-//   edad: Int
-// }
-// type Query {
-//   getPersona(id: ID!): Persona,
-//   getPersonas(campo: String, valor: String): [Persona],
-// }
-// type Mutation {
-//   createPersona(datos: PersonaInput): Persona
-//   updatePersona(id: ID!, datos: PersonaInput): Persona,
-//   deletePersona(id: ID!): Persona,
-// }
-// `);
-
-const schema = buildSchema(`
-    type Producto {
-        id: ID!,
-        nombre: String,
-        descripcion: String,
-        codigo: Int,
-        imagen: String,
-        precio: Int,
-        stock: Int
-    },
-    input ProductoInput {
-        nombre: String,
-        descripcion: String,
-        codigo: Int,
-        imagen: String,
-        precio: Int,
-        stock: Int
-    },
     type Query {
-        getProducto(id: ID!): Producto,
-        getProductos(campo: String, valor: String): [Producto],
-    },
-    type Mutation {
-        createProducto(datos: ProductoInput): Producto
-    },
-`);
+        allProducts: [Product!]!
+    }
 
-module.exports = { Producto,schema }
+    type Mutation {
+        createProduct(nombre: String, descripcion: String,  codigo: String, imagen: String, precio: Int, stock: Int): Product!
+        productById(_id: String): Product
+        productDeleteById(_id: String): Product
+    }
+`;
+
+module.exports = typeDefs 
