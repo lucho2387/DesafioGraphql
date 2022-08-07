b96d74596f44c56482fea3077ff610475a1593f9
lucho2387 <88002884+lucho2387@users.noreply.github.com>
2022-08-06 21:09:43 -0300

    Update resolvers.js

--- src/controllers/resolvers.js
+++ src/controllers/resolvers.js
@@ -1,1 +1,30 @@
+const Product  = require('../models/model/product')
 
+const resolvers = {
+    Query: {
+        allProducts: async (parent, args) => {
+            const products = await Product.find()
+            return products
+        }
+    },
+    Mutation: {
+        createProduct: async (parent, args) => {
+            const product = await Product.create(args)
+            product._id = product._id.toString()
+            return product
+        },
+
+        productById: async (parent, args) => {
+            const products = await Product.findById(args._id)
+            return products
+        },
+
+        productDeleteById: async (parent, args) => {
+            const { _id } = args
+            const productsDelete = await Product.deleteOne({_id})
+            return productsDelete
+        }
+    }
+}
+
+module.exports = resolvers
